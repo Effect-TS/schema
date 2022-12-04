a4d7bf609a2805ead13ece01b604bb3e3852d747
gcanti <giulio.canti@gmail.com>
2022-12-04 12:46:22 +0100

    add benchmark

---
+++ benchmark/index.ts
@@ -0,0 +1,116 @@
+import * as JD from "@fp-ts/schema/JsonDecoder"
+import * as t from "@fp-ts/schema/Schema"
+import * as Benchmark from "benchmark"
+
+/*
+io-ts
+space-object (good) x 476,424 ops/sec ±0.45% (92 runs sampled)
+space-object (bad) x 434,563 ops/sec ±0.58% (87 runs sampled)
+schema (Chunk)
+space-object (good) x 148,078 ops/sec ±4.17% (86 runs sampled)
+space-object (bad) x 238,464 ops/sec ±5.36% (80 runs sampled)
+*/
+
+const suite = new Benchmark.Suite()
+
+const Vector = t.tuple(t.number, t.number, t.number)
+
+const Asteroid = t.struct({
+  type: t.literal("asteroid"),
+  location: Vector,
+  mass: t.number
+})
+
+const Planet = t.struct({
+  type: t.literal("planet"),
+  location: Vector,
+  mass: t.number,
+  population: t.number,
+  habitable: t.boolean
+})
+
+const Rank = t.union(
+  t.literal("captain"),
+  t.literal("first mate"),
+  t.literal("officer"),
+  t.literal("ensign")
+)
+
+const CrewMember = t.struct({
+  name: t.string,
+  age: t.number,
+  rank: Rank,
+  home: Planet
+})
+
+const Ship = t.struct({
+  type: t.literal("ship"),
+  location: Vector,
+  mass: t.number,
+  name: t.string,
+  crew: t.array(CrewMember)
+})
+
+const T = t.union(Asteroid, Planet, Ship)
+
+const decoder = JD.jsonDecoderFor(T)
+
+const good = {
+  type: "ship",
+  location: [1, 2, 3],
+  mass: 4,
+  name: "foo",
+  crew: [
+    {
+      name: "bar",
+      age: 44,
+      rank: "captain",
+      home: {
+        type: "planet",
+        location: [5, 6, 7],
+        mass: 8,
+        population: 1000,
+        habitable: true
+      }
+    }
+  ]
+}
+
+const bad = {
+  type: "ship",
+  location: [1, 2, "a"],
+  mass: 4,
+  name: "foo",
+  crew: [
+    {
+      name: "bar",
+      age: 44,
+      rank: "captain",
+      home: {
+        type: "planet",
+        location: [5, 6, 7],
+        mass: 8,
+        population: "a",
+        habitable: true
+      }
+    }
+  ]
+}
+
+// console.log(T.decode(good))
+// console.log(T.decode(bad))
+
+suite
+  .add("space-object (good)", function() {
+    decoder.decode(good)
+  })
+  .add("space-object (bad)", function() {
+    decoder.decode(bad)
+  })
+  .on("cycle", function(event: any) {
+    console.log(String(event.target))
+  })
+  .on("complete", function(this: any) {
+    console.log("Fastest is " + this.filter("fastest").map("name"))
+  })
+  .run({ async: true })
