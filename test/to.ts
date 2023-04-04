5ac50196c2eb593e0f3e9bafc7cffeaeff8d9712
gcanti <giulio.canti@gmail.com>
2023-04-04 02:04:46 +0200

    chore

--- test/to.ts
+++ test/to.ts
@@ -3,16 +3,28 @@ import * as S from "@effect/schema/Schema"
 
 describe.concurrent("to", () => {
   it("transform", () => {
-    const to_ = S.tuple(S.numberFromString(S.string), S.numberFromString(S.string))
     const schema = pipe(
       S.string,
       S.transform(
-        to_,
+        S.tuple(S.numberFromString(S.string), S.numberFromString(S.string)),
         (s) => [s, s] as const,
         ([s]) => s
-      )
+      ),
+      S.to
     )
-    const to = S.to(schema)
-    expect(S.parse(to)([1, 2])).toEqual([1, 2])
+    expect(S.parse(schema)([1, 2])).toEqual([1, 2])
+  })
+
+  it("refinement", () => {
+    const schema = pipe(
+      S.string,
+      S.minLength(1),
+      S.maxLength(2),
+      S.to
+    )
+    expect(S.is(schema)("")).toEqual(false)
+    expect(S.is(schema)("a")).toEqual(true)
+    expect(S.is(schema)("ab")).toEqual(true)
+    expect(S.is(schema)("abc")).toEqual(false)
   })
 })
