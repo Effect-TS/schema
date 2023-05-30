5bbb5aa88b7096c51b9d86e938bcce4b698358d9
gcanti <giulio.canti@gmail.com>
2023-05-30 12:48:08 +0200

    chore

--- test/Transform/union.ts
+++ test/Transform/union.ts
@@ -1,3 +1,4 @@
+import * as PR from "@effect/schema/ParseResult"
 import * as S from "@effect/schema/Schema"
 import * as Util from "@effect/schema/test/util"
 import * as T from "@effect/schema/Transform"
@@ -7,5 +8,16 @@ describe.concurrent("union", () => {
     const transform = T.union(S.number, T.NumberFromString)
     await Util.expectParseSuccess(transform, 1, 1)
     await Util.expectParseSuccess(transform, "2", 2)
+  })
+
+  it("should go on with the next member", async () => {
+    const fail = T.transformResult(
+      S.number,
+      S.number,
+      () => PR.failure(PR.forbidden),
+      PR.success
+    )
+    const transform = T.union(fail, S.number)
+    await Util.expectParseSuccess(transform, 1, 1)
   })
 })
