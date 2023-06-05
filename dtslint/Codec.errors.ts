6019bb6abca8457c886f2995840d0630fd68cd9d
gcanti <giulio.canti@gmail.com>
2023-06-05 20:00:37 +0200

    switch to definitelytyped/dtslint

--- dtslint/Codec.errors.ts
+++ dtslint/Codec.errors.ts
@@ -2,11 +2,11 @@ import { pipe } from "@effect/data/Function";
 import * as S from "@effect/schema/Schema";
 import * as T from "@effect/schema/Codec";
 
-// $ExpectError
+// @ts-expect-error
 pipe(S.boolean, S.optional, S.nullable)
 
-// $ExpectError
+// @ts-expect-error
 pipe(S.boolean, T.optional, T.nullable)
 
-// $ExpectError
+// @ts-expect-error
 S.struct({ a: S.string, b: S.number, c: T.optional(S.boolean).withDefault(() => true) });
