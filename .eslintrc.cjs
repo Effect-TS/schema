298cda72a4039d105b39ebdf39b525ba0ecbc95c
Tim <hello@timsmart.co>
2023-08-04 11:21:57 +1200

    update build tools

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,7 +21,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@repo-tooling/dprint/recommended"
+    "plugin:@effect/recommended"
   ],
   plugins: ["deprecation", "import", "sort-destructure-keys", "simple-import-sort", "codegen"],
   rules: {
@@ -63,7 +63,7 @@ module.exports = {
     "@typescript-eslint/no-array-constructor": "off",
     "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/no-namespace": "off",
-    "@repo-tooling/dprint/dprint": [
+    "@effect/dprint": [
       "error",
       {
         config: {
