1fb1002fe3c76865401183cb093654c7e72c0193
Tim <hello@timsmart.co>
2023-10-10 16:44:16 +1300

    update effect (#476)

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -2,6 +2,7 @@ import * as Fs from "node:fs";
 
 (JSON.parse(Fs.readFileSync("package.json")).files ?? [])
   .filter((_) => _ !== "src")
+  .concat(["src/tsconfig.json"])
   .forEach((file) => {
     Fs.rm(file, { recursive: true, force: true }, () => {});
   });
