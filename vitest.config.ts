035efff45eb1839244402ab7cab3283f635b8cba
gcanti <giulio.canti@gmail.com>
2023-10-24 15:58:11 +0200

    use vitest imports

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   test: {
     include: ["./test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     exclude: [],
-    globals: true,
     coverage: {
       provider: "v8"
     }
